d1ade271df1586637132688afc0f934bda8535f7
DilanSandaruwan <56243618+DilanSandaruwan@users.noreply.github.com>
2020-01-03 21:12:19 +0530

    Successfully updated for more variables

---
+++ app/src/main/java/com/example/databasewithapi1/DBHelper.java
@@ -0,0 +1,29 @@
+package com.example.databasewithapi1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "MyWeather.db";
+    public static final String TABLE_NAME = "test1";
+
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + "( );";
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/example/databasewithapi1/MainActivity.java
+++ app/src/main/java/com/example/databasewithapi1/MainActivity.java
@@ -5,21 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_check = (Button)findViewById(R.id.button);
+    //Button btn_check = (Button)findViewById(R.id.button);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,44 +27,67 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkWeatherHere(View view)
     {
-        btn_check.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        EditText txt_city = (EditText)findViewById(R.id.txt_city);
 
-            }
-        });
-        String city = btn_check.getText().toString();
-        if(!city.equals("")){
-            String url = "http://api.weatherapi.com/v1/forecast.json?key=49cff2e634f64d949d6165415200101&q="+city+"&days=2";
-            Ion.with(this)
-                    .load(url)
-                    .asString()
-                    .setCallback(new FutureCallback<String>() {
-                        @Override
-                        public void onCompleted(Exception e, String result) {
-                            Log.i("RESTAPI","cat dsta " + result);
-                            displayInfo(result);
-                        }
-                    });
-        }else{
-            Toast.makeText(this, "Please enter the city to check weather.", Toast.LENGTH_SHORT).show();
-        }
+        Log.i("checkWeather Entered","Looking here");
+        String city = String.valueOf(txt_city.getText());
+        String url = "http://api.weatherapi.com/v1/current.json?key=a4ba9fa04ed74e31a46150533200201&q="+city+"";
+        //"http://samples.openweathermap.org/data/2.5/weather?q=London,uk"
+        Log.i("checkWeather Entered","String here");
+        Ion.with(this)
+                .load(url)
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        Log.i("chuck","Chuck joke is "+ result);
+                        displayInfo(result);
+                    }
+                });
+
+        Log.i("checkWeather Entered","Finished here");
+        //Toast.makeText(this, "Please enter the city to check weather.", Toast.LENGTH_SHORT).show();
+
+        //}
 
 
     }
-    private void displayInfo(String result)
+    private void displayInfo(String data)
     {
+        Log.i("displayInfo.","Entered here");
+        Toast.makeText(this, "Entered into displayInfo.", Toast.LENGTH_SHORT).show();
         try
         {
-            JSONObject jsonObject = new JSONObject(result);
-            JSONObject jsonObject1 = jsonObject.getJSONObject("location");
-            String title = jsonObject1.getString("name");
-            TextView output = (TextView)findViewById(R.id.output);
-            output.setText(title);
+                Log.i("displayInfo.","try 1 here");
+            JSONObject jsonObject = new JSONObject(data);
+                Log.i("displayInfo.","try 2 here");
+            JSONObject j_location = jsonObject.getJSONObject("location");
+            JSONObject j_current = jsonObject.getJSONObject("current");
+            JSONObject j_condition = j_current.getJSONObject("condition");
+
+                Log.i("displayInfo.","Middle here");
+
+            Toast.makeText(this, "hail me", Toast.LENGTH_SHORT).show();
+
+            String str_name = j_location.getString("name");
+            String str_temp_c = j_current.getString("temp_c");
+            String str_wind = j_current.getString("wind_kph");
+            String str_cond = j_condition.getString("text");
+
+            TextView output_city = findViewById(R.id.txtv_output_city);
+            TextView output_temp = findViewById(R.id.txtv_output_temp);
+            TextView output_wind = findViewById(R.id.txtv_output_wind);
+
+            output_city.setText(str_name);
+            output_temp.setText(str_temp_c);
+            output_wind.setText(str_wind);
 
         }catch(JSONException e){
+            Toast.makeText(this, "Exceptioned into displayInfo.", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
+        Log.i("displayInfo.","Finished here");
+        Toast.makeText(this, "Exited from displayInfo.", Toast.LENGTH_SHORT).show();
     }
 
 }
